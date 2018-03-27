6fc9f56fa249ef6d106ab10e1d702cfba33d27dc
Weiren Xie <>
2018-03-27 11:11:05 +0200

    keys

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ function fetchSettings(callback) {
 			});
         } else {
            AWS.config.update({
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ""
            });
         }
 
